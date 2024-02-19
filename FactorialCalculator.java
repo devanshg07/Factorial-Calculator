95f93499314f8d8a372875f8bae46ac01f134f9b
devanshg07 <devanshgoyal087@gmail.com>
2024-02-19 12:10:11 -0500

    added while loop

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -4,30 +4,34 @@ public class FactorialCalculator{
     public static void main(String[]args){
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 200 to exit");
 
-        int num = getNum();
-        int factorialNum = calcFactorial(num);
+        while(true){
+        double num = getNum();
+        double factorialNum = calcFactorial(num);
 
         System.out.println("Factorial: " + num + "  =  " + factorialNum);
-
-        
+        }        
     }
 
-    public static int getNum(){
+    public static double getNum(){
         Scanner scanner = new Scanner(System.in);
 
-        int num = 0;
+        double num = 0;
 
         System.out.print("Enter the whole number to factorialize: ");
-        num = scanner.nextInt();
+        num = scanner.nextDouble();
+        if(num == 100.0){
+            System.exit(0);
+        }
 
         return num;
     }
 
-    public static int calcFactorial(int num){
-        int factorialNum = num;
+    public static double calcFactorial(double num){
+        double factorialNum = num;
 
-        for(int i = (num - 1); i >= 2; i--){
+        for(double i = (num - 1.0); i >= 2.0; i--){
             factorialNum = factorialNum * i;
         }
 
